4c12d6a0d3bc7556c33fa89cf9170ee091a4086c
Gholibjon Qasobov <qasobovgholib@gmail.com>
2023-02-05 09:05:05 +0600

    lab_3

--- lab_3/classes/task4.py
+++ lab_3/classes/task4.py
@@ -1,5 +1,28 @@
+from math import sqrt
+
+
 class Point:
     x = None
     y = None
 
-    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def show(self):
+        print(self.x, self.y)
+
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
+    def dist(self, x, y):
+        d = sqrt(pow((self.x - x), 2) + pow((self.y - y), 2))
+        print(d)
+
+
+figure1 = Point(4, 5)
+
+figure1.move(4, 5)
+figure1.show()
+figure1.dist(2, 3)
--- lab_3/function/task2.py
+++ lab_3/function/task2.py
@@ -4,4 +4,4 @@ F = int(input("Enter Fahrenheit temperature: "))
 def to_centigrade(F):
     return (5 / 9) * (F - 32)
 
-# print(to_centigrade(F))
+print(to_centigrade(F))
